294e0db84f15f1253434cf5521f6bf45714f54c6
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-06-05 15:59:49 -0700

    Trying out testrail

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,8 @@
 
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import tsconfigPaths from 'vite-tsconfig-paths'
-/*
-export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: 'jsdom',
-    },
-    resolve: {
-        alias: [
-            {
-                find: "react-redux/es/exports",
-                replacement: resolve(__dirname, "./node_modules/react-redux/lib/exports")
-            }
-        ]
-    }
-})*/
+
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     test: {
