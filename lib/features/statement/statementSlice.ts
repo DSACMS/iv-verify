6a896d28f5c9436c608818a0bdd249127f6ad57e
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-06-09 10:32:41 -0700

    Screen to set signed statement

---
+++ lib/features/statement/statementSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction} from '@reduxjs/toolkit'
+import type { RootState } from '@/lib/store'
+
+export interface SignedStatementState {
+    name?: string
+    lastDayOfWork?: Date
+    amount?: number
+    lastPayment?: Date
+}
+
+export const initialState: SignedStatementState = { }
+
+
+export const statementSlice = createSlice({
+    name: 'statement/set',
+    initialState,
+    reducers: {
+        setSignedStatement: (state, action: PayloadAction<SignedStatementState>) => {
+            state = action.payload
+        }
+    }
+})
+
+export const { setSignedStatement } = statementSlice.actions
+export const selectSignedStatement = (state: RootState) => state.statement
+
+export default statementSlice.reducer
