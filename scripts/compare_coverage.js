0ec7c3ac3ed5bad57e2fe958f5f7100458f9f464
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-06-10 11:33:48 -0700

    FFS-890: Start computing code coverage for PRs

---
+++ scripts/compare_coverage.js
@@ -0,0 +1,46 @@
+module.exports = ({github, context, MAIN_WORKFLOW_RUN_SHA}) => {
+    const fs = require('fs')
+
+    function computeTotalCoverage(data) {
+        let totalLines = 0
+        let coveredLines = 0
+        for (const fileName of data) {
+            const lines = data[fileName].s
+            totalLines += lines.length
+            const coveredLinesForFile = lines.filter((line) => line > 0)
+            coveredLines += coveredLinesForFile.length
+        }
+
+        if (totalLines == 0) {
+            return 0
+        }
+
+        return Math.floor((coveredLines / totalLines) * 100)
+    }
+
+    fs.readFile("coverage/coverage.json", function(err, data) {
+        if (err) { throw err }
+
+        const prCoverageData = JSON.parse(data)
+
+        fs.readFile(`verify-prototype-covearge-${MAIN_WORKFLOW_RUN_SHA}.json`, function(err, data) {
+            if (err) { throw err }
+
+            const mainCoverageData = JSON.parse(data)
+
+            const prTotalCoverage = computeTotalCoverage(prCoverageData)
+            const mainTotalCoverage = computeTotalCoverage(mainCoverageData)
+
+            if (prTotalCoverage > mainTotalCoverage) {
+                // Coverage increased
+                console.log("Coverage Increased")
+            } else if (prCoverageData < mainTotalCoverage) {
+                // Coverage decreased
+                console.log("Coverage decreased")
+            } else {
+                // Coverage is the same
+                console.log("Covearge is the same")
+            }
+        })
+    })
+}
