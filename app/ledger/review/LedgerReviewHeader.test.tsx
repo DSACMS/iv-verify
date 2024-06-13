3df133a406a548e27fa0b1fbf654d2e0868c1a25
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-06-13 11:25:40 -0700

    Tests for LedgerReviewHeader

---
+++ app/ledger/review/LedgerReviewHeader.test.tsx
@@ -0,0 +1,123 @@
+import { afterEach, beforeEach, describe, expect, it, test } from 'vitest'
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import LedgerReviewHeader from './LedgerReviewHeader'
+import { makeStore } from '@/lib/store'
+import { vi } from 'vitest'
+import { EnhancedStore } from '@reduxjs/toolkit'
+import mockRouter from 'next-router-mock'
+import { BenefitsState } from '@/lib/features/benefits/benefitsSlice'
+
+describe('Ledger Review Header', async () => {
+    const SNAP_INCOME = 350.00
+    const MEDICAID_INCOME = 412.00
+
+    afterEach(cleanup)
+
+    describe('Medicaid and Snap', () => {
+        let benefits: BenefitsState
+        beforeEach(() => {
+            benefits = {
+                standardDeduction: true,
+                deductionAmount: 50,
+                medicaid: true,
+                snap: true,
+            }
+
+            const store: EnhancedStore = makeStore()
+            render (<Provider store={store}><LedgerReviewHeader benefits={benefits} snapIncomeTotal={SNAP_INCOME} medicaidIncomeTotal={MEDICAID_INCOME} /></Provider>)
+        })
+
+        it('Displays SNAP and Medicaid header', () => {
+            expect(screen.getByTestId('review-header')).toBeDefined()
+        })
+
+        it('Displays SNAP Income', () => {
+            expect(screen.getByText(`SNAP Income: $${SNAP_INCOME}`)).toBeDefined()
+        })
+
+        it('Displays Medicaid Income', () => {
+            expect(screen.getByText(`Medicaid Income: $${MEDICAID_INCOME}`)).toBeDefined()
+        })
+
+        it('Does not display Medicaid only header', () => {
+            expect(screen.queryByTestId("medicaid-only-header")).toBeNull()
+        })
+
+        it('Does not display SNAP only header', () => {
+            expect(screen.queryByTestId("snap-only-header")).toBeNull()
+        })
+    })
+
+    describe('Medicaid Only', () => {
+        let benefits: BenefitsState
+        beforeEach(() => {
+            benefits = {
+                medicaid: true,
+                snap: false,
+            }
+
+            const store: EnhancedStore = makeStore()
+            render (<Provider store={store}><LedgerReviewHeader benefits={benefits} snapIncomeTotal={SNAP_INCOME} medicaidIncomeTotal={MEDICAID_INCOME} /></Provider>)
+        })
+
+        it('Displays Medicaid only header', () => {
+            expect(screen.getByTestId('medicaid-only-header')).toBeDefined()
+        })
+
+        it('Does not display SNAP and Medicaid header', () => {
+            expect(screen.queryByTestId('review-header')).toBeNull()
+        })
+
+        it('Does not display SNAP Income', () => {
+            const header = screen.getByTestId('medicaid-only-header')
+            expect(header.firstChild?.textContent?.indexOf(`${SNAP_INCOME}`)).toEqual(-1)
+        })
+
+        it('Displays Medicaid Income', () => {
+            const header = screen.getByTestId('medicaid-only-header')
+            expect(header.firstChild?.textContent?.indexOf(`${MEDICAID_INCOME}`)).toBeGreaterThanOrEqual(0)
+        })
+
+        it('Does not display SNAP only header', () => {
+            expect(screen.queryByTestId("snap-only-header")).toBeNull()
+        })
+    })
+
+    describe('SNAP Only', () => {
+        let benefits: BenefitsState
+        beforeEach(() => {
+            benefits = {
+                standardDeduction: true,
+                deductionAmount: 50,
+                medicaid: false,
+                snap: true,
+            }
+
+            const store: EnhancedStore = makeStore()
+            render (<Provider store={store}><LedgerReviewHeader benefits={benefits} snapIncomeTotal={SNAP_INCOME} medicaidIncomeTotal={MEDICAID_INCOME} /></Provider>)
+        })
+
+        it('Does not display Medicaid only header', () => {
+            expect(screen.queryByTestId('medicaid-only-header')).toBeNull()
+        })
+
+        it('Does not display SNAP and Medicaid header', () => {
+            expect(screen.queryByTestId('review-header')).toBeNull()
+        })
+
+        it('Does display SNAP Income', () => {
+            const header = screen.getByTestId('snap-only-header')
+            expect(header.firstChild?.textContent?.indexOf(`${SNAP_INCOME}`)).toBeGreaterThanOrEqual(0)
+        })
+
+        it('Does not display Medicaid Income', () => {
+            const header = screen.getByTestId('snap-only-header')
+            expect(header.firstChild?.textContent?.indexOf(`${MEDICAID_INCOME}`)).toEqual(-1)
+        })
+
+        it('Does display SNAP only header', () => {
+            expect(screen.queryByTestId("snap-only-header")).toBeDefined()
+        })
+    })
+})
--- app/ledger/review/LedgerReviewHeader.tsx
+++ app/ledger/review/LedgerReviewHeader.tsx
@@ -1,5 +1,5 @@
 import { BenefitsState } from "@/lib/features/benefits/benefitsSlice"
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "@/app/i18n/client"
 
 interface ReviewHeaderProps {
     benefits: BenefitsState
@@ -13,7 +13,7 @@ export default function LedgerReviewHeader({benefits, snapIncomeTotal, medicaidI
     if (benefits.medicaid && benefits.snap) {
         return (
             <div>
-                <h3 className="margin-bottom-3">{t("review_header")}</h3>
+                <h3 className="margin-bottom-3" data-testid="review-header">{t("review_header")}</h3>
                 <h4>{t("review_snap_income", {amount:snapIncomeTotal})}</h4>
                 <h4>{t("review_medicaid_income", {amount:medicaidIncomeTotal})}</h4>
                 <p className="margin-top-3">{t("review_ledger_sent_medicaid_snap")}</p>
@@ -22,14 +22,14 @@ export default function LedgerReviewHeader({benefits, snapIncomeTotal, medicaidI
     } else if (benefits.medicaid) {
         return (
             <div>
-                <h3 className="margin-bottom-3">{t("review_medicaid_only_income", {amount: medicaidIncomeTotal})}</h3>
+                <h3 className="margin-bottom-3" data-testid="medicaid-only-header">{t("review_medicaid_only_income", {amount: medicaidIncomeTotal})}</h3>
                 <p>{t("review_ledger_sent_medicaid_only")}</p>
             </div>
         )
     } else if (benefits.snap) {
         return (
             <div>
-                <h3 className="margin-bottom-3">{t("review_snap_only_income", {amount: snapIncomeTotal})}</h3>
+                <h3 className="margin-bottom-3" data-testid="snap-only-header">{t("review_snap_only_income", {amount: snapIncomeTotal})}</h3>
                 <p>{t("review_ledger_sent_snap_only")}</p>
             </div>
         )
