198e7cdfa03193d9d28cc24a26382a6af51c3d7e
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-06-13 09:55:38 -0700

    remove testing code

--- app/ledger/review/page.tsx
+++ app/ledger/review/page.tsx
@@ -3,17 +3,15 @@ import "@trussworks/react-uswds/lib/uswds.css"
 import "@trussworks/react-uswds/lib/index.css"
 import { Header, Title, Button,  Grid, GridContainer } from '@trussworks/react-uswds' 
 import { useTranslation } from '@/app/i18n/client'
-import { useAppDispatch, useAppSelector } from "@/lib/hooks"
+import { useAppSelector } from "@/lib/hooks"
 import { useRouter } from "next/navigation"
-import { BenefitsState, selectBenefits, setBenefits } from "@/lib/features/benefits/benefitsSlice"
-import { IncomeItem, addIncome, selectIncomeTotal } from "@/lib/features/ledger/income/incomeSlice"
+import { selectBenefits } from "@/lib/features/benefits/benefitsSlice"
+import { selectIncomeTotal } from "@/lib/features/ledger/income/incomeSlice"
 import Link from "next/link"
 import IncomeList from "@/app/components/IncomeList"
 import ExpenseList from "@/app/components/ExpenseList"
 import LedgerReviewHeader from "./LedgerReviewHeader"
 import SnapExpenses from "./SnapExpenses"
-import { useEffect } from "react"
-import { ExpenseItem, addExpense } from "@/lib/features/ledger/expenses/expensesSlice"
 
 const DAY_COUNT = 30
 
@@ -22,55 +20,10 @@ export default function Page() {
     const router = useRouter()
     const benefits = useAppSelector(state => selectBenefits(state))
     const incomeTotal = useAppSelector(state => selectIncomeTotal(state))
-    const dispatch = useAppDispatch()
 
     function continueButtonClicked() {
         router.push("/statement/sign")
     }
-
-    useEffect(() => {
-        const benefitsState: BenefitsState = {
-            standardDeduction: true,
-            deductionAmount: 50,
-            medicaid: true,
-            snap: false,
-        }
-        dispatch(setBenefits(benefitsState))
-
-        const income: IncomeItem[] = [
-            {
-                amount: 300,
-                name: "Yardwork",
-                description: ""
-            }, {
-                amount: 200,
-                name: "House Cleaning",
-                description: ""
-            }
-        ]
-
-        dispatch(addIncome(income[0]))
-        dispatch(addIncome(income[1]))
-
-        const expense: ExpenseItem[] = [
-            {
-                amount: 35,
-                date: new Date(),
-                name: "Gas",
-                expenseType: "Gas",
-                isMileage: false,
-            }, {
-                amount: 105,
-                date: new Date(),
-                name: "Clothing",
-                expenseType: "Clothing",
-                isMileage: false,
-            }
-        ]
-
-        dispatch(addExpense(expense[0]))
-        dispatch(addExpense(expense[1]))
-    }, [])
 
     return (
         <div>
