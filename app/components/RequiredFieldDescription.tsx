eee968b4bd688c8e1c11aa39c8bb2af1f0b491cc
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-06-24 06:48:16 -0700

    fix

--- app/components/RequiredFieldDescription.tsx
+++ app/components/RequiredFieldDescription.tsx
@@ -3,7 +3,7 @@ import { RequiredMarker } from "@trussworks/react-uswds"
 import { Trans } from 'react-i18next'
 
 export default function RequiredFieldDescription() {
-    const { t } = useTranslation('en')
+    const { t } = useTranslation()
     return (
         <div className="margin-top-1">
             <Trans i18nKey="required_field_description" t={t} components={[<RequiredMarker key='asterisk' />]} />
