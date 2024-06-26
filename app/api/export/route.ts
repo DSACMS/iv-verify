e7d17899a51e157dc4374ddf7e4f4f3a42d6f4c2
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-06-26 14:31:51 -0700

    remove debugging

--- app/api/export/route.ts
+++ app/api/export/route.ts
@@ -4,7 +4,6 @@ import { readFile } from 'fs/promises'
 export const dynamic = 'force-dynamic'
 export async function POST(request: Request) {
     const data = await request.formData()
-    console.log(data)
 
     const arrayBuffer = await readFile('app/api/export/form3.pdf')
     const doc = await PDFDocument.load(arrayBuffer)
