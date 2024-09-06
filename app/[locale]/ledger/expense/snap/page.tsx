2eb2c3e9d61438122d420b856187cd49d9eef55f
Kate Green <kathryn.green@omb.eop.gov>
2024-09-06 10:38:05 -0400

    fix snap radio button from FFS-932/PR #57.

--- app/[locale]/ledger/expense/snap/page.tsx
+++ app/[locale]/ledger/expense/snap/page.tsx
@@ -1,114 +1,72 @@
 'use client'
 
-import { Accordion, Button, Grid, GridContainer, HeadingLevel, Radio, Form } from '@trussworks/react-uswds' 
+import { Button, Form, FormGroup, Grid, GridContainer, Radio } from '@trussworks/react-uswds' 
 import { useRouter } from "next/navigation"
 import { useTranslation } from "react-i18next"
-import { Controller, SubmitHandler, useForm } from "react-hook-form"
-import { useAppDispatch, useAppSelector } from "@/lib/hooks"
-import { BenefitsState, selectBenefits, setBenefits } from "@/lib/features/benefits/benefitsSlice"
-import { selectExpenseTotal } from "@/lib/features/ledger/expenses/expensesSlice"
-import { useEffect } from "react"
-import { selectRecommendStandardDeduction } from "@/lib/store"
+import { SubmitHandler, useForm } from "react-hook-form"
 import VerifyNav from "@/app/components/VerifyNav"
 
 export default function Page() {
     const { t } = useTranslation()
     const router = useRouter()
-    const dispatch = useAppDispatch()
-    const benefits = useAppSelector(state => selectBenefits(state))
-    const expenseTotal = useAppSelector(state => selectExpenseTotal(state))
-    const reccommendStandardDeduction = useAppSelector(state => selectRecommendStandardDeduction(state))
 
-    interface FormData {
-        take_deduction: string
-        do_not_take_deduction: string
+    const MONTHLY_AMOUNT = "XXX.XX"
+
+    type FormData = {
+        snapRadio: boolean
     }
 
     const {
         handleSubmit,
-        control
+        setValue
     } = useForm<FormData>()
 
     const onSubmit: SubmitHandler<FormData> = (data => {
-        if (data.take_deduction == "on" || data.do_not_take_deduction == "on") {
-            let newBenefits = {...benefits} as BenefitsState
-            if (data.take_deduction == "on") {
-                newBenefits.standardDeduction = true
-            } else if (data.do_not_take_deduction == "on") {
-                newBenefits.standardDeduction = false
-            }
+        const { snapRadio } = data
 
-            dispatch(setBenefits(newBenefits))
+        if (snapRadio == true) {
+            router.push("/ledger/expense")
+        } else if (snapRadio == false) {
             router.push("/ledger/review")
         }
     })
 
-    const items = [
-      {
-        title: t('snap_deduction_accordion_header'),
-        content: (
-          <div>
-            <pre>{t('snap_deduction_accordion_body')}</pre>
-          </div>
-        ),
-        expanded: false,
-        id: 'snap_deduction',
-        headingLevel: 'h4' as HeadingLevel,
-      }
-    ]
-
-    useEffect(() => {
-        if (!benefits.snap || !reccommendStandardDeduction) {
-            // User should not have been pushed to this screen
-            return router.push('/ledger/review')
-        }
-    }, [benefits.snap, reccommendStandardDeduction, router])
-
     return (
         <div>
-            <VerifyNav title={t('snap_deduction_title')} />
+            <VerifyNav title={t('expenses_snap_standard_title')} />
             <div className="usa-section">
                 <GridContainer>
                     <Grid row gap>
                         <main className="usa-layout-docs">
                             <Form onSubmit={handleSubmit(onSubmit)}>
-                                <h3 className="margin-bottom-2" data-testid="snap_deduction_header">{t('snap_deduction_header', {amount: expenseTotal})}</h3>
+                                <h3 className="margin-bottom-2" data-testid="expense-snap-header">{t('expenses_snap_standard_header', {amount: MONTHLY_AMOUNT})}</h3>
+                                <span className="usa-hint">{t('expenses_snap_standard_subheader')}</span>
 
-                                <span className="usa-hint">{t('snap_deduction_subheader')}</span>
-
-                                <Accordion multiselectable={true} className="margin-top-2" items={items} />
-
-                                <Controller
-                                    name="take_deduction"
-                                    control={control}
-                                    render={({ field }) => 
-                                        <Radio 
-                                            id="take_deduction_radio" {...field} 
-                                            label={t('snap_deduction_take_header')} 
-                                            labelDescription={t('snap_deduction_take_body')} 
-                                            tile 
-                                            className="margin-top-5" 
-                                            data-testid="take_deduction_radio" 
-                                            value="on" 
-                                        />
-                                    }
-                                />
-                                <Controller
-                                    name="do_not_take_deduction"
-                                    control={control}
-                                    render={({ field }) => 
-                                        <Radio 
-                                            id="do_not_take_deduction_radio" {...field} 
-                                            label={t('snap_deduction_do_not_take_header')} 
-                                            tile 
-                                            data-testid="do_not_take_deduction_radio" 
-                                            value="on" 
-                                        />
-                                    }
-                                />
+                                <FormGroup>
+                                    <Radio 
+                                        id="take_deduction_radio"
+                                        name="snap-radio" 
+                                        label={t('snap_deduction_take_header')} 
+                                        labelDescription={t('snap_deduction_take_body')} 
+                                        tile 
+                                        className="margin-top-5" 
+                                        data-testid="take_deduction_radio" 
+                                        value="on" 
+                                        onChange={(e) => setValue("snapRadio", true)}
+                                    />
+                                    <Radio 
+                                        id="do_not_take_deduction_radio"
+                                        name="snap-radio"
+                                        label={t('snap_deduction_do_not_take_header')} 
+                                        tile 
+                                        data-testid="do_not_take_deduction_radio" 
+                                        value="on" 
+                                        onChange={(e) => setValue("snapRadio", false)}
+                                    />
+                                </FormGroup>
 
                                 <p className="text-center margin-top-5">
-                                    <Button type="submit" data-testid="continue-button">{t('snap_deduction_continue_button')}</Button>
+                                    <Button type="submit" data-testid="continue-button">{t('expenses_snap_standard_continue')}</Button>
                                 </p>
                             </Form>
                         </main>
