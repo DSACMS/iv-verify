02d0b3bf5114c3b73219e94e5a0bc1541c960ced
Kate Green <kathryn.green@omb.eop.gov>
2024-10-04 17:04:38 -0400

    Add edit generation functionality

--- test/fixtures/generator.ts
+++ test/fixtures/generator.ts
@@ -1,15 +1,50 @@
 import { createUuid } from "@/lib/store"
 
+import { BenefitsState } from "@/lib/features/benefits/benefitsSlice"
 import { SetExpensePayload, ExpenseItem } from "@/lib/features/job/expenses/expensesSlice"
 import { SetJobPayload, JobItem } from "@/lib/features/job/jobSlice"
 import { SetPaymentPayload, PaymentItem } from "@/lib/features/job/payment/paymentSlice"
 
+/**
+ * emptyStateObject
+ * Object
+ * 
+ * returns an empty state object for normalized state [job, expense, payment]
+ */
 export const emptyStateObject = {
   byId: {},
   allIds: []
 }
 
-export const generateExpense = (jobId: string, payload?: ExpenseItem) => {
+
+/**
+ * generateBenefits
+ * Generate benefits fixture
+ * 
+ * @param payload 
+ * @returns BenefitsState
+ */
+export const generateBenefits = (payload?: BenefitsState) => {
+  const defaultPayload = {
+    snap: true,
+    medicaid: false,
+    standardDeduction: false,
+    deductionAmount: 0
+  } as BenefitsState
+
+  return payload ? payload : defaultPayload
+}
+
+/**
+ * generateExpense
+ * Generates expenses with a new or existing job id, an optional custom payload, and new or existing expenseId
+ * 
+ * @param jobId UUID
+ * @param payload? ExpenseItem
+ * @param expenseId? UUID
+ * @returns SetExpensePayload
+ */
+export const generateExpense = (jobId: string, payload?: ExpenseItem, expenseId?: string) => {
   const defaultPayload = {
     job: jobId,
     name: 'Gas',
@@ -20,25 +55,42 @@ export const generateExpense = (jobId: string, payload?: ExpenseItem) => {
   } as ExpenseItem
 
   return {
-      id: createUuid(),
+      id: expenseId ? expenseId : createUuid(),
       item: payload ? payload : defaultPayload
   } as SetExpensePayload
 }
 
-export const generateJob = (payload?: JobItem) => {
+/**
+ * generateJob 
+ * Generate new or existing job objects and an optional custom payload
+ * 
+ * @param payload? JobItem
+ * @param jobId? UUID
+ * @returns SetJobPayload
+ */
+export const generateJob = (payload?: JobItem, jobId?: string) => {
   const defaultPayload = {
-    description: 'A description2',
+    description: 'A description',
     business: '',
     taxesFiled: false
   } as JobItem
 
   return {
-      id: createUuid(),
+      id: jobId ? jobId : createUuid(),
       item: payload ? payload : defaultPayload
   } as SetJobPayload
 }
 
-export const generatePayment = (jobId: string, payload?: PaymentItem) => {
+/**
+ * generatePayment
+ * Generates payments with a new or existing job id, an optional custom payload, and new or existing paymentId
+ * 
+ * @param jobId UUID
+ * @param payload? ExpenseItem
+ * @param paymentId? UUID
+ * @returns SetPaymentPayload
+ */
+export const generatePayment = (jobId: string, payload?: PaymentItem, paymentId?: string) => {
   const defaultPayload = {
     job: jobId,
     amount: 10,
@@ -47,7 +99,7 @@ export const generatePayment = (jobId: string, payload?: PaymentItem) => {
   } as PaymentItem
 
   return {
-      id: createUuid(),
+      id: paymentId ? paymentId : createUuid(),
       item: payload ? payload : defaultPayload
   } as SetPaymentPayload
 }
